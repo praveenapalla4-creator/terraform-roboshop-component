054eb3702e815200b68baa2afdbb17af0b6a33d3
praveena <praveenapalla4@gmail.com>
2026-01-26 19:59:38 +0530

    terraformcomponent maodule

---
+++ bootstrap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+environment=$2
+component=$1
+dnf install ansible -y
+
+REPO_URL=https://github.com/praveenapalla4-creator/ansible-roboshop-roles-tf.git
+REPO_DIR=/opt/roboshop/ansible
+ANSIBLE_DIR=ansible-roboshop-roles-tf
+
+
+mkdir -p $REPO_DIR
+mkdir -p /var/log/roboshop/
+touch ansible.log
+
+
+cd $REPO_DIR
+#chekc if anisible repo is already closed or not
+
+if [ -d $ANSIBLE_DIR ]; then
+  
+  cd $ANSIBLE_DIR
+  git pull
+else 
+    git clone $REPO_URL
+    cd $ANSIBLE_DIR
+fi
+
+ansible-playbook -e component=$component  -e env=$environment main.yaml
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+    type=string
+    default="roboshop"
+}
+variable "environment" {
+    type=string
+    default="dev"
+}
+
+variable "domain_name"{
+    default="daws86s.help"
+}
+variable "component"{
+    
+}
+variable "rule_priority" {
+
+  
+}
