05bb69569cdf2a6f822a796365f331c59dcd6ef1
praveena <praveenapalla4@gmail.com>
2026-01-27 11:15:36 +0530

    ports to80

--- data.tf
+++ data.tf
@@ -19,7 +19,7 @@ data "aws_ami" "joindevops" {
 }
 
 data "aws_ssm_parameter" "sg_id" {
-  name = "/${var.project_name}/${var.environment}/${var.component}_sg_id"
+  name = "/${var.project_name}/${var.environment}/${var.component}/sg_id"
 }
 
 data "aws_ssm_parameter" "vpc_id" {
