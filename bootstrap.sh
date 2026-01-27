ded9c608fe67ede7c054456976e10a8bfedcb224
praveena <praveenapalla4@gmail.com>
2026-01-27 15:45:45 +0530

    handlers

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,13 @@
 
 environment=$2
 component=$1
-dnf install ansible -y
-# dnf install python3-pip -y
-# pip3 install boto3 botocore amazon.aws
+#!/bin/bash
+
+# Install Ansible and Python dependencies
+dnf install -y ansible python3-pip
+pip3 install --upgrade pip
+pip3 install boto3 botocore
+
 
 REPO_URL=https://github.com/praveenapalla4-creator/ansible-roboshop-roles-tf.git
 REPO_DIR=/opt/roboshop/ansible
@@ -27,5 +31,5 @@ else
     git clone $REPO_URL
     cd $ANSIBLE_DIR
 fi
-
+echo "environmnet is :$2"
 ansible-playbook -e component=$component  -e env=$environment main.yaml
