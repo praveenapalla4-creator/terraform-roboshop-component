b3304dcffb5675abb49f9c8d4915d9bc2dd7bb71
praveena <praveenapalla4@gmail.com>
2026-01-28 12:42:07 +0530

    handlers

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "terraform_data" "main" {
   provisioner "remote-exec" {
     inline = [
      "chmod +x /tmp/bootstrap.sh",
-    "sudo sh /tmp/bootstrap.sh ${var.component} ${var.environment}"
+    "sudo sh /tmp/bootstrap.sh ${var.component} dev"
 
     ]
     
